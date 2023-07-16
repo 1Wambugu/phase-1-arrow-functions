7ba56aa79e6705f725a6faea922e653c7950421e
Martin <martingakuya@gmail.com>
2023-07-16 07:44:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression: divide
+const divide = function(a, b) {
+  return a / b;
+}
+
+console.log(divide(2000, 100)); // output: 20
+
+// Funtion expression: Square
+const square = (num) => {
+  return num * num;
+}
+// Arrow function: add
+const add = (x,y) => {
+    return x+y;
+}
